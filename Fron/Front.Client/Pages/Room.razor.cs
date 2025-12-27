63dfe2651b61d592abed78a6a685cae9cf14e8bd
Дмитрий Богатырев <dbogatirev@joytech.store>
2025-12-27 18:14:50 +0700

    send message (local)

--- Fron/Front.Client/Pages/Room.razor.cs
+++ Fron/Front.Client/Pages/Room.razor.cs
@@ -1,13 +1,45 @@
+using Fron.Client.ViewModels;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using MudBlazor;
+using Shared.Model;
 
 namespace Fron.Client.Pages;
 
 public partial class Room : ComponentBase
 {
-    private string _message = null!;
-
-    private void SendMessage()
+    private List<ChatMessageViewModel> _messages = new();
+    
+    private EditForm _form = new();
+    private ChatMessageEditFormModel _model = new();
+    private MudTextField<string> _messageInput = new();
+    
+    /// <summary>
+    /// Метод отправки сообщения.
+    /// Пока что локальный.
+    /// </summary>
+    private async Task SendMessage()
     {
-        
+        if (!string.IsNullOrWhiteSpace(_model.Message))
+        {
+            var msg = new ChatMessageViewModel(_model.Message);
+            _messages.Add(msg);
+            
+            // Очищаем поле ввода
+            _model = new();
+            
+            StateHasChanged();
+
+            // Обязательно должен быть delay. Иначе фокус не установится
+            await Task.Delay(1);
+            
+            await _messageInput.FocusAsync();
+        }
     }
+}
+
+
+public class ChatMessageEditFormModel
+{
+    public string? Message = null!;
 }
---
+++ Fron/Front.Client/ViewModels/ChatMessageViewModel.cs
@@ -0,0 +1,13 @@
+namespace Fron.Client.ViewModels;
+
+
+public record ChatMessageViewModel
+{
+    public string Text { get; init; }
+    public DateTime Timestamp { get; init; } = DateTime.Now;
+    
+    public ChatMessageViewModel(string text)
+    {
+        Text = text;
+    }
+}
