4f3149bd4b72ea8b6a5dfe1e3a1eab00f638cc4a
Дмитрий Богатырев <dbogatirev@joytech.store>
2025-12-27 20:49:52 +0700

    ru language

---
+++ FreeVoice.Client/Pages/Room.razor.cs
@@ -0,0 +1,44 @@
+using FreeVoice.Client.ViewModels;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using MudBlazor;
+
+namespace FreeVoice.Client.Pages;
+
+public partial class Room : ComponentBase
+{
+    private List<ChatMessageViewModel> _messages = new();
+    
+    private EditForm _form = new();
+    private ChatMessageEditFormModel _model = new();
+    private MudTextField<string> _messageInput = new();
+    
+    /// <summary>
+    /// Метод отправки сообщения.
+    /// Пока что локальный.
+    /// </summary>
+    private async Task SendMessage()
+    {
+        if (!string.IsNullOrWhiteSpace(_model.Message))
+        {
+            var msg = new ChatMessageViewModel(_model.Message);
+            _messages.Add(msg);
+            
+            // Очищаем поле ввода
+            _model = new();
+            
+            StateHasChanged();
+
+            // Обязательно должен быть delay. Иначе фокус не установится
+            await Task.Delay(1);
+            
+            await _messageInput.FocusAsync();
+        }
+    }
+}
+
+
+public class ChatMessageEditFormModel
+{
+    public string? Message = null!;
+}
