76c2a4e05eafc31467642288d8c836169b514005
Дмитрий Богатырев <dbogatirev@joytech.store>
2025-12-27 17:54:42 +0700

    add room razor cs

---
+++ Fron/Front.Client/Pages/Room.razor.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Fron.Client.Pages;
+
+public partial class Room : ComponentBase
+{
+    private string _message = null!;
+
+    private void SendMessage()
+    {
+        
+    }
+}
